dcc46116043c0fa0e09516b612413d4a694bdda5
KonH <konh@yandex.ru>
2019-02-25 21:16:08 +0700

    Prepare Unity project for external sources compilation

---
+++ UnityClient/Assets/Scripts/Editor/BuildMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+namespace UnityClient.Editor {
+	public static class BuildMenu {
+		static BuildUtility _utility = new BuildUtility("GameLogics", "Assets/Plugins/GameLogics/netstandard2.0");
+
+		[MenuItem("Build/Configuration/Debug")]
+		public static void Configuration_Debug() {
+			_utility.BuildForConfiguration("Debug");
+		}
+
+		[MenuItem("Build/Configuration/Release")]
+		public static void Configuration_Release() {
+			_utility.BuildForConfiguration("Release");
+		}
+	}
+}
---
+++ UnityClient/Assets/Scripts/Editor/BuildUtility.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using Debug = UnityEngine.Debug;
+
+namespace UnityClient.Editor {
+	public class BuildUtility {
+		public string ProjectPath { get; }
+		public string AssetPath   { get; }
+
+		public BuildUtility(string projectPath, string assetPath) {
+			ProjectPath = projectPath;
+			AssetPath   = assetPath;
+		}
+
+		public void BuildForConfiguration(string name) {
+			var curDir  = Directory.GetCurrentDirectory();
+			var parent  = Directory.GetParent(curDir);
+			var path    = Path.Combine(parent.FullName, ProjectPath);
+			var outPath = $"{curDir}/{AssetPath}";
+
+			if ( Run($"clean \"{path}\" -o \"{outPath}\"") && Run($"build \"{path}\" -c {name} -o \"{outPath}\"") ) {
+				AssetDatabase.Refresh();
+			}
+		}
+
+		static bool Run(string command) {
+			// Hack, because %PATH% inside Unity may be different on MacOS
+			var oldPath = Environment.GetEnvironmentVariable("PATH");
+			Environment.SetEnvironmentVariable("PATH", oldPath + ":/usr/local/share/dotnet/");
+
+			var startInfo = new ProcessStartInfo {
+				FileName               = "dotnet",
+				Arguments              = command,
+				RedirectStandardError  = true,
+				RedirectStandardOutput = true,
+				UseShellExecute        = false,
+				CreateNoWindow         = true
+			};
+
+			try {
+				using ( var process = Process.Start(startInfo) ) {
+					process.WaitForExit();
+					var output = process.StandardOutput.ReadToEnd() + "\n" + process.StandardError.ReadToEnd();
+					if ( process.ExitCode == 0 ) {
+						Debug.Log($"Success: '{command}'\n{output}");
+						return true;
+					} else {
+						Debug.LogError($"Failed: '{command}'\n{output}");
+					}
+				}
+			} catch ( Exception e ) {
+				Debug.LogError($"Failed: '{command}': {e}");
+			}
+			return false;
+		}
+	}
+}
