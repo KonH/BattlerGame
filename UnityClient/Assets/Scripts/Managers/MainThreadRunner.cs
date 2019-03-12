ef8762ca2eaba36b72b8e99a3aa6ac9bb6da3189
KonH <konh@yandex.ru>
2019-03-12 21:25:29 +0700

    Main thread runner to execute tasks on unity thread

---
+++ UnityClient/Assets/Scripts/Managers/MainThreadRunner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityClient.Managers {
+	public class MainThreadRunner : MonoBehaviour {
+		TaskScheduler _scheduler;
+		
+		void Awake() {
+			_scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+		}
+
+		public void Run(Func<Task> task) {
+			Task.Factory.StartNew(task, CancellationToken.None, TaskCreationOptions.None, _scheduler);
+		}
+	}
+
+}
