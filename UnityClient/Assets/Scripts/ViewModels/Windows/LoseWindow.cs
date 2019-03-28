c1287b174e71c064fc5d5b16f55e4a86e216b754
KonH <konh@yandex.ru>
2019-03-28 18:42:24 +0700

    Remove duplicated code and re-use animation

--- UnityClient/Assets/Scripts/ViewModels/Windows/LoseWindow.cs
+++ UnityClient/Assets/Scripts/ViewModels/Windows/LoseWindow.cs
@@ -1,36 +1,19 @@
 ﻿using System;
-using TMPro;
-using UnityClient.Models;
-using UnityClient.Utils;
-using UnityEngine;
+using UnityClient.ViewModels.Windows.Animations;
 using UnityEngine.UI;
-using Zenject;
 
 namespace UnityClient.ViewModels.Windows {
 	public class LoseWindow : BaseWindow {
-		public float     ShowDuration;
-		public float     HideDuration;
-		public Transform MessageRoot;
-		public Button    OkButton;
+		public Button OkButton;
+
+		public BaseAnimation Animation;
 		
 		void Awake() {
-			MessageRoot.localScale = Vector3.zero;
-			MessageRoot.DoScale(ShowDuration, 1.0f).Detach();
-		}
-
-		[Inject]
-		public void Init() {
-			
+			Animation.Show();
 		}
 
 		public void Show(Action callback) {
-			OkButton.onClick.AddListener(() => HideThenClose(callback));
-		}
-
-		void HideThenClose(Action callback) {
-			OkButton.interactable = false;
-			MessageRoot.DoScale(HideDuration, 0.0f).GetAwaiter()
-				.OnCompleted(callback);
+			OkButton.onClick.AddListener(() => Animation.Hide(callback));
 		}
 	}
 }
--- UnityClient/Assets/Scripts/ViewModels/Windows/NoticeWindow.cs
+++ UnityClient/Assets/Scripts/ViewModels/Windows/NoticeWindow.cs
@@ -1,35 +1,24 @@
-﻿using System;
-using TMPro;
+﻿using TMPro;
 using UnityClient.Models;
-using UnityClient.Utils;
-using UnityEngine;
+using UnityClient.ViewModels.Windows.Animations;
 using UnityEngine.UI;
 
 namespace UnityClient.ViewModels.Windows {
 	public class NoticeWindow : BaseWindow {
-		public float     ShowDuration;
-		public float     HideDuration;
-		public Transform MessageRoot;
 		public TMP_Text  MessageText;
 		public Button    OkButton;
 		public Button    CloseButton;
 
+		public BaseAnimation Animation;
+		
 		void Awake() {
-			MessageRoot.localScale = Vector3.zero;
-			MessageRoot.DoScale(ShowDuration, 1.0f).Detach();
+			Animation.Show();
 		}
 
 		public void Show(NoticeModel model) {
 			MessageText.text = model.Message;
-			OkButton.onClick.AddListener(() => HideThenCallback(model.Callback, true));
-			CloseButton.onClick.AddListener(() => HideThenCallback(model.Callback, false));
-		}
-
-		void HideThenCallback(Action<bool> callback, bool result) {
-			OkButton.interactable = false;
-			CloseButton.interactable = false;
-			MessageRoot.DoScale(HideDuration, 0.0f).GetAwaiter()
-				.OnCompleted(() => callback(result));
+			OkButton.onClick.AddListener(() => Animation.Hide(() => model.Callback(true)));
+			CloseButton.onClick.AddListener(() => Animation.Hide(() => model.Callback(false)));
 		}
 	}
 }
