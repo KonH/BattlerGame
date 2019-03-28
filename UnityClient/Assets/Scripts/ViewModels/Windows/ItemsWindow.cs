315e5936f832a621f473a516ead2cb4c08f31ad6
KonH <konh@yandex.ru>
2019-03-28 21:31:53 +0700

    Items window mock

---
+++ UnityClient/Assets/Scripts/ViewModels/Windows/ItemsWindow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityClient.ViewModels.Windows.Animations;
+
+namespace UnityClient.ViewModels.Windows {
+	public class ItemsWindow : BaseWindow {
+		public Button CloseButton;
+
+		public BaseAnimation Animation;
+		
+		void Awake() {
+			Animation.Show();
+		}
+
+		public void Show() {
+			CloseButton.onClick.AddListener(() => Animation.Hide(() => Destroy(gameObject)));
+		}
+	}
+}
