15384549659475522e1ebee7a69391ab22c1aba0
KonH <konh@yandex.ru>
2019-04-09 18:10:18 +0700

    Feature: item upgrade

--- GameLogics/Shared/Models/Configs/Item/BaseItemConfig.cs
+++ GameLogics/Shared/Models/Configs/Item/BaseItemConfig.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
+
 namespace GameLogics.Shared.Models.Configs {
 	public abstract class BaseItemConfig {
 		public virtual ItemType Type { get; }
+
+		public Dictionary<Resource, int>[] UpgradePrice { get; set; }
 	}
 }
