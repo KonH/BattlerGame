a1fa36e6d8043fb58f0b0525fcf18c9326f5ca29
KonH <konh@yandex.ru>
2019-04-10 18:34:58 +0700

    Feature: level up command

---
+++ GameLogics/Shared/Commands/LevelUpCommand.cs
@@ -0,0 +1,47 @@
+﻿using GameLogics.Shared.Commands.Base;
+using GameLogics.Shared.Models.Configs;
+using GameLogics.Shared.Models.State;
+
+namespace GameLogics.Shared.Commands {
+	public sealed class LevelUpCommand : IInternalCommand {
+		public readonly ulong UnitId;
+
+		public LevelUpCommand(ulong unitId) {
+			UnitId = unitId;
+		}
+
+		public bool IsValid(GameState state, Config config) {
+			if ( !state.Units.TryGetValue(UnitId, out var unit) ) {
+				return false;
+			}
+			if ( unit.Level >= config.UnitLevels.Length ) {
+				return false;
+			}
+			if ( unit.Experience < config.UnitLevels[unit.Level] ) {
+				return false;
+			}
+			return true;
+		}
+
+		public void Execute(GameState state, Config config, ICommandBuffer buffer) {
+			var unit = state.Units[UnitId];
+			var oldLevel = unit.Level;
+			unit.Level++;
+			unit.Experience -= config.UnitLevels[oldLevel];
+			var isLastLevel = (unit.Level == config.UnitLevels.Length);
+			if ( isLastLevel ) {
+				unit.Experience = 0;
+			}
+			
+			buffer.Add(new HealUnitCommand(UnitId));
+
+			if ( !isLastLevel && (unit.Experience >= config.UnitLevels[unit.Level]) ) {
+				buffer.Add(new LevelUpCommand(UnitId));
+			}
+		}
+
+		public override string ToString() {
+			return $"{nameof(LevelUpCommand)} ({UnitId})";
+		}
+	}
+}
---
+++ UnitTests/LevelUpCommandTest.cs
@@ -0,0 +1,77 @@
+﻿using GameLogics.Shared.Commands;
+using GameLogics.Shared.Models.Configs;
+using GameLogics.Shared.Models.State;
+using Xunit;
+
+namespace UnitTests {
+	public sealed class LevelUpCommandTest : BaseCommandTest<LevelUpCommand> {
+		readonly ulong _id;
+
+		public LevelUpCommandTest() {
+			_config.AddUnit("unit", new UnitConfig {
+				BaseDamage = new int[] { 1, 2, 3 },
+				MaxHealth  = new int[] { 1, 2, 3 }
+			});
+			_id = _state.NewEntityId();
+			_config.UnitLevels = new int[] { 100, 200 };
+			var unit = new UnitState("unit", 1).WithId(_id);
+			unit.Experience = 110;
+			_state.AddUnit(unit);
+		}
+
+		[Fact]
+		void CantLevelUpUnknownUnit() {
+			IsInvalid(new LevelUpCommand(ulong.MaxValue));
+		}
+
+		[Fact]
+		void CantLevelUpIfNoEnoughExperience() {
+			_state.Units[_id].Experience = 99;
+
+			IsInvalid(new LevelUpCommand(_id));
+		}
+
+		[Fact]
+		void CantLevelUpIfNoMoreLevels() {
+			_state.Units[_id].Experience = 300;
+			Execute(new LevelUpCommand(_id));
+
+			_state.Units[_id].Experience = int.MaxValue;
+			IsInvalid(new LevelUpCommand(_id));
+		}
+
+		[Fact]
+		void CantBeRunDirectly() {
+			IsInvalidOnServer(new LevelUpCommand(_id));
+		}
+
+		[Fact]
+		void IsLevelIncreased() {
+			Execute(new LevelUpCommand(_id));
+
+			Assert.Equal(1, _state.Units[_id].Level);
+		}
+
+		[Fact]
+		void IsUnitHealed() {
+			Execute(new LevelUpCommand(_id));
+
+			Assert.Equal(2, _state.Units[_id].Health);
+		}
+
+		[Fact]
+		void IsRestOfExperienceSaved() {
+			Execute(new LevelUpCommand(_id));
+
+			Assert.Equal(10, _state.Units[_id].Experience);
+		}
+
+		[Fact]
+		void IsExperienceWastedOnLastLevel() {
+			_state.Units[_id].Experience = 350;
+			Execute(new LevelUpCommand(_id));
+
+			Assert.Equal(0, _state.Units[_id].Experience);
+		}
+	}
+}
