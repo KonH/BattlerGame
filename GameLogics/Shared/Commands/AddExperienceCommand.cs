e090efda43a0e427a63fe889024ffd8831627ef1
KonH <konh@yandex.ru>
2019-04-10 18:56:39 +0700

    Feature: add experience command

---
+++ GameLogics/Shared/Commands/AddExperienceCommand.cs
@@ -0,0 +1,40 @@
+﻿using GameLogics.Shared.Commands.Base;
+using GameLogics.Shared.Models.Configs;
+using GameLogics.Shared.Models.State;
+
+namespace GameLogics.Shared.Commands {
+	public sealed class AddExperienceCommand : IInternalCommand {
+		public readonly ulong UnitId;
+		public readonly int   Amount;
+
+		public AddExperienceCommand(ulong unitId, int amount) {
+			UnitId = unitId;
+			Amount = amount;
+		}
+
+		public bool IsValid(GameState state, Config config) {
+			if ( Amount <= 0 ) {
+				return false;
+			}
+			if ( !state.Units.TryGetValue(UnitId, out var unit) ) {
+				return false;
+			}
+			if ( unit.Level == config.UnitLevels.Length ) {
+				return false;
+			}
+			return true;
+		}
+
+		public void Execute(GameState state, Config config, ICommandBuffer buffer) {
+			var unit = state.Units[UnitId];
+			unit.Experience += Amount;
+			if ( unit.Experience >= config.UnitLevels[unit.Level] ) {
+				buffer.Add(new LevelUpCommand(UnitId));
+			}
+		}
+
+		public override string ToString() {
+			return $"{nameof(AddExperienceCommand)} ({UnitId}, {Amount})";
+		}
+	}
+}
---
+++ UnitTests/AddExperienceCommandTest.cs
@@ -0,0 +1,63 @@
+﻿using GameLogics.Shared.Commands;
+using GameLogics.Shared.Models.Configs;
+using GameLogics.Shared.Models.State;
+using Xunit;
+
+namespace UnitTests {
+	public sealed class AddExperienceCommandTest : BaseCommandTest<AddExperienceCommand> {
+		readonly ulong _id;
+
+		public AddExperienceCommandTest() {
+			_config.AddUnit("unit", new UnitConfig {
+				BaseDamage = new int[] { 1, 2, 3 },
+				MaxHealth  = new int[] { 1, 2, 3 }
+			});
+			_id = _state.NewEntityId();
+			_config.UnitLevels = new int[] { 100, 200 };
+			var unit = new UnitState("unit", 1).WithId(_id);
+			_state.AddUnit(unit);
+		}
+
+		[Fact]
+		void CantAddExperienceToUnknownUnit() {
+			IsInvalid(new AddExperienceCommand(ulong.MaxValue, 10));
+		}
+
+		[Fact]
+		void CantAddInvalidExperienceAmount() {
+			IsInvalid(new AddExperienceCommand(_id, -1));
+			IsInvalid(new AddExperienceCommand(_id, 0));
+		}
+
+		[Fact]
+		void CantAddOnLastLevel() {
+			_state.Units[_id].Level = 2;
+
+			IsInvalid(new AddExperienceCommand(_id, 10));
+		}
+
+		[Fact]
+		void CantBeRunDirectly() {
+			IsInvalidOnServer(new AddExperienceCommand(_id, 10));
+		}
+
+		[Fact]
+		void IsExperienceIncreased() {
+			Execute(new AddExperienceCommand(_id, 10));
+
+			Assert.Equal(10, _state.Units[_id].Experience);
+		}
+
+		[Fact]
+		void IsLevelUpProduced() {
+			Produces<LevelUpCommand>(new AddExperienceCommand(_id, 100));
+		}
+
+		[Fact]
+		void CanLevelUpSeveralSteps() {
+			Execute(new AddExperienceCommand(_id, 300));
+
+			Assert.Equal(2, _state.Units[_id].Level);
+		}
+	}
+}
