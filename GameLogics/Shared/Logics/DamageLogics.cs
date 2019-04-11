47d11bf0bf0bf137a459f1ab48f6d07d135bba11
KonH <konh@yandex.ru>
2019-04-11 17:46:51 +0700

    Core: public access to damage/absorb calculation logic

--- GameLogics/Shared/Logics/DamageLogics.cs
+++ GameLogics/Shared/Logics/DamageLogics.cs
@@ -4,7 +4,7 @@ using GameLogics.Shared.Models.Configs;
 using GameLogics.Shared.Models.State;
 
 namespace GameLogics.Shared.Logics {
-	static class DamageLogics {
+	public static class DamageLogics {
 		public static int GetDamage(GameState state, Config config, ulong dealerId, ulong targetId) {
 			var baseDamage   = GetBaseDamage(state, config, dealerId);
 			var weaponDamage = GetWeaponDamage(state, config, dealerId);
@@ -13,12 +13,12 @@ namespace GameLogics.Shared.Logics {
 			return Math.Max(damage, 0);
 		}
 
-		static int GetBaseDamage(GameState state, Config config, ulong unitId) {
+		public static int GetBaseDamage(GameState state, Config config, ulong unitId) {
 			var unitState = GetUnitState(state, unitId);
 			return config.Units[unitState.Descriptor].BaseDamage[unitState.Level];
 		}
 
-		static int GetWeaponDamage(GameState state, Config config, ulong unitId) {
+		public static int GetWeaponDamage(GameState state, Config config, ulong unitId) {
 			var items = GetUnitState(state, unitId).Items;
 			var accum = 0;
 			foreach ( var item in items ) {
@@ -30,7 +30,7 @@ namespace GameLogics.Shared.Logics {
 			return accum;
 		}
 
-		static int GetAbsorb(GameState state, Config config, ulong unitId) {
+		public static int GetAbsorb(GameState state, Config config, ulong unitId) {
 			var items = GetUnitItems(state, unitId);
 			var accum = 0;
 			foreach ( var item in items ) {
@@ -43,6 +43,9 @@ namespace GameLogics.Shared.Logics {
 		}
 
 		static UnitState GetUnitState(GameState state, ulong unitId) {
+			if ( state.Units.TryGetValue(unitId, out var unit) ) {
+				return unit;
+			}
 			return state.Level.FindUnitById(unitId);
 		}
 
