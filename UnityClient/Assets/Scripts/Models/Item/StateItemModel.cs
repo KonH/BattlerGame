1b4dab76d513bd138c8609e770bb8ff35334f5f5
KonH <konh@yandex.ru>
2019-04-11 17:17:52 +0700

    UI: show item stats

--- UnityClient/Assets/Scripts/Models/Item/StateItemModel.cs
+++ UnityClient/Assets/Scripts/Models/Item/StateItemModel.cs
@@ -7,9 +7,17 @@ namespace UnityClient.Models {
 		
 		public StateItemModel(ItemState state, BaseItemConfig config, ClickAction<ItemModel> onClick) {
 			Type    = config.Type;
-			Name    = $"{state.Descriptor}, lvl.{state.Level + 1} ({config.Type})";
+			Name    = $"{state.Descriptor}, lvl.{state.Level + 1} ({config.Type}, +{GetItemValue(state, config)})";
 			OnClick = onClick;
 			State   = state;
+		}
+
+		string GetItemValue(ItemState state, BaseItemConfig config) {
+			switch ( config ) {
+				case WeaponConfig w: return w.Damage[state.Level].ToString();
+				case ArmorConfig a:  return a.Absorb[state.Level].ToString();
+				default:             return "?";
+			}
 		}
 	}
 }
