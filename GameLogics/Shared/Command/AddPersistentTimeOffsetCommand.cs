7df4c72f4ce99f9bdad946fdee61507b1932f2d9
KonH <konh@yandex.ru>
2019-04-16 18:43:38 +0700

    Feature: debug command to advance persistent time offset

---
+++ GameLogics/Shared/Command/AddPersistentTimeOffsetCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GameLogics.Shared.Command.Base;
+using GameLogics.Shared.Model.Config;
+using GameLogics.Shared.Model.State;
+
+namespace GameLogics.Shared.Command {
+	public sealed class AddPersistentTimeOffsetCommand : IDebugCommand {
+		public readonly TimeSpan Amount;
+
+		public AddPersistentTimeOffsetCommand(TimeSpan amount) {
+			Amount = amount;
+		}
+
+		public bool IsValid(GameState state, ConfigRoot config) => true;
+
+		public void Execute(GameState state, ConfigRoot config, ICommandBuffer buffer) {
+			state.Time.PersistentOffset += Amount;
+		}
+
+		public override string ToString() {
+			return $"{nameof(AddPersistentTimeOffsetCommand)} ({Amount})";
+		}
+	}
+}
